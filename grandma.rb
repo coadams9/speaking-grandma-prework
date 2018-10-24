5c89b835fa1d7c96e56a81e723a8f126a50ead9a
Cory Adams <coadams9@gmail.com>
2018-10-24 10:35:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -19,6 +19,6 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else prase != phrase.upcase
-    return 
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
