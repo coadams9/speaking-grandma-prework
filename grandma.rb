e520c055a0205b70d070857c2e41dea28bb5d17b
Cory Adams <coadams9@gmail.com>
2018-10-24 10:36:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else prase != phrase.upcase
+  else phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
